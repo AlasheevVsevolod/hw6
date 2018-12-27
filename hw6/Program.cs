5cadbf9e72bff74ffb198e6c3d43234d56d88cdc
Vsevolod <2007juggernaut@gmail.com>
2018-12-27 19:11:52 +0300

    task1

--- hw6/Program.cs
+++ hw6/Program.cs
@@ -10,6 +10,21 @@ namespace hw6
 	{
 		static void Main(string[] args)
 		{
+			//B6 - P1 / 6. 1DReadConsoleMassive
+			int[] newArr = new int[6];
+			for (int i = 0; i < newArr.Length; i++)
+			{
+				Console.Write($"Введите число №{i}: ");
+				newArr[i] = Convert.ToInt32(Console.ReadLine());
+			}
+
+			Console.WriteLine();
+			for (int i = 0; i < newArr.Length; i++)
+			{
+				Console.WriteLine($"newArr[{i}]: {newArr[i]}");
+			}
+
+			Console.Read();
 		}
 	}
 }
